78744b0831a9fddb2386fdef6ca4f96aeccdbc7a
Sean Scholte <Seanscholte15@outlook.com>
2022-10-16 17:24:43 +0200

    what level am i even on?

---
+++ Controller/Data.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Model;
+namespace Controller
+{
+    
+    public static class Data
+    {
+        public static Competition Competition { get; set; }
+        public static Race CurrentRace { get; set; }
+
+        public static void Initialize(){
+            Competition = new Competition();
+            AddParticipants();
+            AddTracks();
+        }
+
+        public static void AddParticipants()
+        { 
+            List<IParticipant> _list = new List<IParticipant> {
+                new Driver("Razor",0, new Car(1, 1200, 300, false) ,TeamColor.Blue),
+                new Driver("Bull", 0, new Car(1, 1200, 300, false),TeamColor.Grey),
+                new Driver("Ronnie", 0, new Car(1, 1200, 300, false),TeamColor.Red)
+            };
+            Competition.Participants = _list;
+        }
+
+        public static void AddTracks()
+        {
+            SectionTypes[] SectionsA = new SectionTypes[] {
+                SectionTypes.StartGrid,
+                SectionTypes.Straight,
+                SectionTypes.LeftCorner,
+                SectionTypes.RightCorner,
+                SectionTypes.Straight,
+                SectionTypes.Finish
+            };
+
+            SectionTypes[] SectionsB = new SectionTypes[] {
+                SectionTypes.StartGrid,
+                SectionTypes.Straight,
+                SectionTypes.LeftCorner,
+                SectionTypes.LeftCorner,
+                SectionTypes.Straight,
+                SectionTypes.RightCorner,
+                SectionTypes.RightCorner,
+                SectionTypes.Straight,
+                SectionTypes.Finish
+            };
+
+            Track _a = new Track("T-1", SectionsA);
+            Track _b = new Track("T-2", SectionsB);
+
+            Queue<Track> tracks = new Queue<Track>();
+            tracks.Enqueue(_a);
+            tracks.Enqueue(_b);
+
+            Competition.Tracks = tracks;
+        }
+
+        public static void NextRace() {
+            Track _track = Competition.NextTrack();
+            if (_track != null) {
+                CurrentRace = new Race(_track, Competition.Participants);
+            }
+        }
+
+
+    }
+}
